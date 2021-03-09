932a3134ad3dc96809035ac1bd33c98e41edd6d3
Daniel Walker <Dw765608@gmail.com>
2021-03-08 21:02:01 -0500

    Finished

--- README.md
+++ README.md
@@ -16,30 +16,30 @@ You have been provided a [JSON object](js/index.js) with all the necessary data 
 
 **Follow these steps to set up and work on your project:**
 
-* [ ] Create a forked copy of this project.
-* [ ] Clone your OWN version of the repository (Not Lambda's by mistake!).
-* [ ] Create a new branch: git checkout -b `<firstName-lastName>`.
-* [ ] Implement the project on your newly created `<firstName-lastName>` branch, committing changes regularly.
-* [ ] Push commits: git push origin `<firstName-lastName>`.
+* [d ] Create a forked copy of this project.
+* [d ] Clone your OWN version of the repository (Not Lambda's by mistake!).
+* [ d] Create a new branch: git checkout -b `<firstName-lastName>`.
+* [ d] Implement the project on your newly created `<firstName-lastName>` branch, committing changes regularly.
+* [ d] Push commits: git push origin `<firstName-lastName>`.
 
 ### Task 2a: MVP
 
 #### Create selectors to point your data into elements
 
-* [ ] Create selectors by using any of the DOM element's methods
-* [ ] Note that IDs have been used on all images. Use the IDs to update src path content
+* d[ ] Create selectors by using any of the DOM element's methods
+* [ d] Note that IDs have been used on all images. Use the IDs to update src path content
 
 #### Update the HTML with the JSON data
 
-* [ ] Remember, NO direct updating of the HTML source is allowed.
-* [ ] Using your selectors, update the content to match the example file.
-* [ ] Remember to update the src attributes on images
+* [ d] Remember, NO direct updating of the HTML source is allowed.
+* [d ] Using your selectors, update the content to match the example file.
+* [d ] Remember to update the src attributes on images
 
 #### Add new content
 
-* [ ] Change the color of the navigation text to be green.
-* [ ] Utilize `.appendChild()` and `.prepend()` to add two new items to the navigation system. You can call them whatever you want.
-* [ ] Check your work by looking at the [original html](original.html) in the browser
+* [d ] Change the color of the navigation text to be green.
+* [ d] Utilize `.appendChild()` and `.prepend()` to add two new items to the navigation system. You can call them whatever you want.
+* [d ] Check your work by looking at the [original html](original.html) in the browser
 
 ### Task 3: Stretch
 
--- js/index.js
+++ js/index.js
@@ -39,4 +39,64 @@ const siteContent = {
 
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+logo.setAttribute('src', siteContent["nav"]["img-src"]);
+const nava = document.querySelectorAll("nav a");
+console.log("s",nava);
+nava[0].textContent= "Services";
+nava[1].textContent= "Product";
+nava[2].textContent= "Vision";
+nava[3].textContent= "Features";
+nava[4].textContent= "About";
+nava[5].textContent= "Contact";
+const topimg = document.querySelector("#cta-img");
+topimg.src = "img/header-img.png";
+const dom= document.querySelector("h1");
+dom.innerHTML = "DOM<br>IS<br>AWESOME";
+button=document.querySelector("button");
+button.textContent="Get Started";
+const he4 = document.querySelectorAll(".text-content h4");
+he4[0].textContent="Features";
+he4[1].textContent="About";
+he4[2].textContent="Services";
+he4[3].textContent="Product";
+he4[4].textContent="Vision";
+const mainp=document.querySelectorAll(".text-content p");
+mainp[0].textContent="Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+mainp[1].textContent="About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+mainp[2].textContent="Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+mainp[3].textContent="Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+mainp[4].textContent="Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+const banner=document.querySelector(".middle-img");
+banner.src="img/mid-page-accent.jpg";
+const conh=document.querySelectorAll(".contact h4");
+conh[0].textContent="CONTACT";
+const conp=document.querySelectorAll(".contact p");
+conp[0].innerHTML="123 Way 456 Street<br>Somewhere, USA";
+conp[1].textContent="1 (888) 888-8888";
+conp[2].textContent="sales@greatidea.io";
+const footer= document.querySelectorAll("footer p");
+footer[0].textContent="Copyright Great Idea! 2018";
+///////////////////////////////////////////////////
+///new-content////
+const nwnav= document.querySelector("header");
+nwnav.style.backgroundColor="green";
+console.log("s",nwnav);
+const Link = document.createElement('a');
+Link.textContent ="Employment";
+Link.href = "#";
+const Link2 = document.createElement('a');
+document.querySelector("nav").appendChild(Link);
+Link2.textContent ="Projects";
+Link2.href = "#";
+document.querySelector("nav").prepend(Link2);
+
+
+
+
+
+
+
+
+
+
+
